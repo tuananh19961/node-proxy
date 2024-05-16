1996e7e8e50a30566a901030490c787074c57d87
tuananh19961 <40750548+tuananh19961@users.noreply.github.com>
2024-05-17 06:01:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const wss = new WebSocket.Server({
   }
 });
 const nodes = {};
-const MAX_CONNECTION_PER_IP = 10;
+const MAX_CONNECTION_PER_IP = 20;
 
 const addToBlackList = async (ip) => {
   try {
