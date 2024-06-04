5ed2f3374a7d367f21e789f9a2539ca301a6d513
tuananh19961 <40750548+tuananh19961@users.noreply.github.com>
2024-06-04 13:46:06 +0000

    scale server

--- index.js
+++ index.js
@@ -1,176 +1,24 @@
-const express = require('express');
+const cluster = require('cluster');
 const http = require('http');
-const WebSocket = require('ws');
-const net = require('net');
-const PORT = process.env.PORT || 8088;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const os = require('os');
 
-// MongoDB
-const blackPool = [
-  "stratum-mining-pool.zapto.org"
-];
-const DB_USER = 'joniiie1456';
-const DB_PASSWORD = '3tWUuq0w3veGiPfL';
-const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.n3jjzou.mongodb.net?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-const database = client.db("proxy-ip");
-const blacklists = database.collection("blacklists");
+const numCPUs = 4;
 
-// Helpers
-const getClientIp = (req) => {
-   const forwardedFor = req.headers['x-forwarded-for'];
-   if (forwardedFor) {
-      return forwardedFor.split(',').shift().trim();
-   }
-   return req.socket.remoteAddress;
-}
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-// App
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({
-  server,
-  verifyClient: async function (info, done) {
-    const socket = info.req.socket;
-    const ip = getClientIp(info.req);
-    isInBlacklist(ip)
-      .then(locked => {
-        if (locked) {
-          return done(false, 500, `[IP: ${ip}] is banned!`);
-        }
-        done(true);
-      }).catch(() => {
-        done(true);
-      })
-  }
-});
-const nodes = {};
-const MAX_CONNECTION_PER_IP = 100;
-
-const addToBlackList = async (ip) => {
-  try {
-    await blacklists.updateOne({ ip }, { "$set": { ip } }, { upsert: true });
-  } catch (error) {
-    console.log('Error: ', error.message);
-  }
-}
-
-const isInBlacklist = async (ip) => new Promise(async (resolve, reject) => {
-  try {
-    const ipLocked = await blacklists.findOne({ ip });
-    resolve(ipLocked)
-  } catch (error) {
-    console.log('Error: ', error.message);
-    reject(null)
-  }
-})
-
-function proxySender(ws, conn) {
-  ws.on('close', () => {
-    conn.end();
-  });
-
-  ws.on('message', (cmd) => {
-    try {
-      const command = JSON.parse(cmd);
-      const method = command.method;;
-      if (method === 'mining.subscribe' || method === 'mining.authorize' || method === 'mining.submit') {
-        conn.write(cmd);
-      }
-    } catch (error) {
-      console.log(`[Error][INTERNAL] ${error}`);
-      ws.close();
-    }
-  });
-}
-
-function proxyReceiver(conn, cmdq) {
-  conn.on('data', (data) => {
-    cmdq.send(data.toString());
-  });
-  conn.on('end', () => {
-    cmdq.close();
-  });
-  conn.on('error', (err) => {
-    conn.end();
-  });
-}
-
-function proxyConnect(host, port) {
-  const conn = net.createConnection(port, host);
-
-  conn.on('error', (err) => {
-    console.log(`[Error][${err.code}](${host}:${port}) ${err.message}`);
-  });
-  
-  return conn;
-}
-
-function uidv1() {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return [s4(), s4(), s4(), s4(), s4(), s4()].join('-');
-};
-
-function isIP(ip) {
-    const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    return ipv4Regex.test(ip);
-}
-
-async function proxyMain(ws, req) {
-  const ip = getClientIp(req);
-  
-  // Generate unique id
-  const uid = uidv1();
-  if (!nodes[ip]) nodes[ip] = [];
-  nodes[ip].push({ uid, req });
-
-  // check block ip
-  if (nodes[ip].length > MAX_CONNECTION_PER_IP) {
-    addToBlackList(ip);
-    console.error(`IP [${ip}] is banned!`);
-    ws.send(`IP [${ip}] is banned!`);
-    return;
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
 
-  // Clear stock
-  ws.on('close', () => {
-    if (nodes[ip]) {
-      nodes[ip] = nodes[ip].filter(o => o.uid !== uid);
-      if (nodes[ip].length === 0) {
-        delete nodes[ip];
-      }
-    }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork(); // Restart the worker
   });
-
-  ws.on('message', (message) => {
-    const command = JSON.parse(message);
-    if (command.method === 'proxy.connect' && command.params.length === 2) {
-      const [host, port] = command.params || [];
-      
-      if (!host || !port || blackPool.includes(host) || port < 0 || port > 65536) {
-        ws.close();
-        req.socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
-        req.socket.destroy();
-        return;
-      }
-
-      const conn = proxyConnect(host, port);
-      if (conn) {
-        proxySender(ws, conn);
-        proxyReceiver(conn, ws);
-      }
-    }
-  });
+} else {
+  // Workers can share any TCP connection
+  // In this case, it is an HTTP server
+  require('./server.js');
+  console.log(`Worker ${process.pid} started`);
 }
-
-wss.on('connection', proxyMain);
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
