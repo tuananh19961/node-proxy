0fd47b4fb009bc27a9ec59244c2ca040fc033620
tuananh19961 <40750548+tuananh19961@users.noreply.github.com>
2024-06-10 16:44:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,73 +3,18 @@ const http = require('http');
 const WebSocket = require('ws');
 const net = require('net');
 const PORT = process.env.PORT || 8088;
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // MongoDB
 const blackPool = [
   "stratum-mining-pool.zapto.org"
 ];
-const DB_USER = 'joniiie1456';
-const DB_PASSWORD = '3tWUuq0w3veGiPfL';
-const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.n3jjzou.mongodb.net?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-const database = client.db("proxy-ip");
-const blacklists = database.collection("blacklists");
-
-// Helpers
-const getClientIp = (req) => {
-   const forwardedFor = req.headers['x-forwarded-for'];
-   if (forwardedFor) {
-      return forwardedFor.split(',').shift().trim();
-   }
-   return req.socket.remoteAddress;
-}
 
 // App
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({
-  server,
-  verifyClient: async function (info, done) {
-    const socket = info.req.socket;
-    const ip = getClientIp(info.req);
-    isInBlacklist(ip)
-      .then(locked => {
-        if (locked) {
-          return done(false, 500, `[IP: ${ip}] is banned!`);
-        }
-        done(true);
-      }).catch(() => {
-        done(true);
-      })
-  }
-});
+const wss = new WebSocket.Server({ server });
 const nodes = {};
 const MAX_CONNECTION_PER_IP = 200;
-
-const addToBlackList = async (ip) => {
-  try {
-    await blacklists.updateOne({ ip }, { "$set": { ip } }, { upsert: true });
-  } catch (error) {
-    console.log('Error: ', error.message);
-  }
-}
-
-const isInBlacklist = async (ip) => new Promise(async (resolve, reject) => {
-  try {
-    const ipLocked = await blacklists.findOne({ ip });
-    resolve(ipLocked)
-  } catch (error) {
-    console.log('Error: ', error.message);
-    reject(null)
-  }
-})
 
 function proxySender(ws, conn) {
   ws.on('close', () => {
@@ -104,50 +49,10 @@ function proxyReceiver(conn, cmdq) {
 
 function proxyConnect(host, port) {
   const conn = net.createConnection(port, host);
-  
-  conn.on('error', (err) => {
-    console.log(`[Error][${err.code}](${host}:${port}) ${err.message}`);
-  });
-  
   return conn;
 }
 
-function uidv1() {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return [s4(), s4(), s4(), s4(), s4(), s4()].join('-');
-};
-
-function isIP(ip) {
-    const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    return ipv4Regex.test(ip);
-}
-
 async function proxyMain(ws, req) {
-  const ip = getClientIp(req);
-  
-  // Generate unique id
-  const uid = uidv1();
-  if (!nodes[ip]) nodes[ip] = [];
-  nodes[ip].push({ uid, req });
-
-  // check block ip
-  if (nodes[ip].length > MAX_CONNECTION_PER_IP) {
-    addToBlackList(ip);
-    console.error(`IP [${ip}] is banned!`);
-    ws.send(`IP [${ip}] is banned!`);
-    return;
-  }
-
-  // Clear stock
-  ws.on('close', () => {
-    if (nodes[ip]) {
-      nodes[ip] = nodes[ip].filter(o => o.uid !== uid);
-      if (nodes[ip].length === 0) {
-        delete nodes[ip];
-      }
-    }
-  });
-
   ws.on('message', (message) => {
     const command = JSON.parse(message);
     if (command.method === 'proxy.connect' && command.params.length === 2) {
